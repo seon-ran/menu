05dd7dd193b41aaf102efe0779b46a1727fc1f28
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2025-06-01 11:56:54 +0900

    feat:cookテーブルからデータを取得するDBスクリプトを作成

---
+++ backend/db.ts
@@ -0,0 +1,29 @@
+require("dotenv").config();
+import { Client } from "pg";
+
+//接続された情報
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+async function connectDb() {
+  try {
+    await client.connect();
+
+    const query = "select * from cook;";
+
+    const result = await client.query(query);
+
+    console.log(result.rows);
+  } catch (err) {
+    console.log("データベース操作中にエラーが発生しました:", err);
+  } finally {
+    await client.end();
+  }
+}
+
+connectDb();
